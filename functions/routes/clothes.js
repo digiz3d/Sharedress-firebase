46f151db3f0b5c9e04383e5d2d43e55d8a577469
Léandre DAUMONT <kekos91@free.fr>
2018-04-23 23:21:37 +0200

    way shorter than before

--- functions/routes/clothes.js
+++ functions/routes/clothes.js
@@ -5,20 +5,10 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 const db = admin.firestore();
 
-function getMultiple(db) {
-    return db.collection('clothes').get()
-        .then(snapshot => {
-            return snapshot;
-        })
-        .catch(err => {
-            throw err;
-        });
-}
-
-
 router.get('/', (req, res, next) => {
 
-    let prom = getMultiple(db);
+    let prom = db.collection('clothes').get();
+
     prom.then((v) => {
         return res.send(v.docs);
     }).catch((e) => {
